ea2e6ae5020fa47777d1aca7aa5b60004bb99db2
Erick Marinho <erickgaves@gmail.com>
2022-07-13 12:01:38 -0300

    :sparkles: Implementando header

--- mcx-icommgroup/src/Components/Header/Header.js
+++ mcx-icommgroup/src/Components/Header/Header.js
@@ -1,6 +1,16 @@
+// import cartImg from "../../assets/cart.png"
+import logo from "../../assets/cart.png";
+import mask from "../../assets/MaskGroup.png"
+
 function Header() {
     return (
-        <p>Eu sou o Header</p>
+        <div className="flex justify-around bg-[#444444] h-[90px] items-center">
+            <div className="flex flex-row items-center">
+                <img src={mask} alt="mask" className="max-h-7 flex items-center pr-2.5" />
+                <p className="text-white text-[26px] font-montserrat tracking-[.20em]">MCX</p>
+            </div>
+            <img src={logo} alt="Cart" className="max-h-14" />
+        </div>
     )
 } 
 
