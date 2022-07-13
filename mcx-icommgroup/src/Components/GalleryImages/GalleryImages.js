ce8d0d76633da00c3a00a83fa2370810aa6b3461
Erick Marinho <erickgaves@gmail.com>
2022-07-13 17:34:19 -0300

    :wrench: Alterando ordem de estilização

--- mcx-icommgroup/src/Components/GalleryImages/GalleryImages.js
+++ mcx-icommgroup/src/Components/GalleryImages/GalleryImages.js
@@ -7,11 +7,15 @@ function GalleryImages() {
             <div className="flex lg:flex-row flex-col">
                 <div className="relative">
                     <img src={image5} alt="modelo" className="lg:pr-[10px] " />
-                    <div className="absolute w-[20px] lg:text-[26px] text-[20px] font-bold tracking-[.20em] text-white bottom-16 translate-x-8 font-montserrat">ÍRIS VERÃO 2022</div>
+                    <div className="absolute w-[20px] lg:text-[26px] text-[20px] font-bold tracking-[.20em] text-white bottom-16 translate-x-8 font-montserrat">
+                        ÍRIS VERÃO 2022
+                    </div>
                 </div>
                 <div className="relative lg:pt-[0px] pt-[20px]">
                     <img src={image6} alt="modelo" className="lg:pl-[10px]" />
-                    <div className="absolute w-[120px] lg:text-[26px] text-[20px] font-bold tracking-[.20em] text-white bottom-20 translate-x-8 font-montserrat">LE LIS BLANC</div>
+                    <div className="absolute w-[120px] lg:text-[26px] text-[20px] font-bold tracking-[.20em] text-white bottom-20 translate-x-8 font-montserrat">
+                        LE LIS BLANC
+                    </div>
                 </div>
             </div>
         </div>
--- mcx-icommgroup/src/Components/Header/Header.js
+++ mcx-icommgroup/src/Components/Header/Header.js
@@ -4,7 +4,7 @@ import mask from "../../assets/MaskGroup.png"
 
 function Header() {
     return (
-        <div className="flex bg-[#444444] h-[90px] items-center lg:justify-around justify-between">
+        <div className="flex bg-[#444444] h-[90px] items-center justify-between lg:justify-around">
             <div className="flex flex-row items-center">
                 <img src={mask} alt="mask" className="max-h-7 flex items-center pr-2.5 pl-2.5" />
                 <p className="text-white text-[26px] font-montserrat font-black tracking-[.20em]">MCX</p>
