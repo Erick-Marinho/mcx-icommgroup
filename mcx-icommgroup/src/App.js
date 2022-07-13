c1220e647eaaf4081087d6a428e1cd8e06c0b26c
Erick Marinho <erickgaves@gmail.com>
2022-07-13 17:33:54 -0300

    :wrench: Implementando componente de destaques

--- mcx-icommgroup/src/App.js
+++ mcx-icommgroup/src/App.js
@@ -1,11 +1,13 @@
 import Header from './Components/Header/Header';
 import GalleryImages from './Components/GalleryImages/GalleryImages'
+import Emphasis from './Components/Emphasis/Emphasis';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <GalleryImages />
+      <Emphasis />
     </div>
   );
 }
---
+++ mcx-icommgroup/src/Components/Emphasis/Emphasis.js
@@ -0,0 +1,9 @@
+function Emphasis() {
+    return (
+        <div className="flex justify-center">
+            <p className="text-[26px] font-nunito pt-[40px]">DESTAQUES</p>
+        </div>
+    )
+}
+
+export default Emphasis;
