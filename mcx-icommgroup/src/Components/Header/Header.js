884ce00d5e9107d259ded02a2554291932aeff69
Erick Marinho <erickgaves@gmail.com>
2022-07-13 15:19:02 -0300

    :hammer: Corrigindo font

--- mcx-icommgroup/src/Components/Header/Header.js
+++ mcx-icommgroup/src/Components/Header/Header.js
@@ -7,7 +7,7 @@ function Header() {
         <div className="flex bg-[#444444] h-[90px] items-center lg:justify-around justify-between">
             <div className="flex flex-row items-center">
                 <img src={mask} alt="mask" className="max-h-7 flex items-center pr-2.5 pl-2.5" />
-                <p className="text-white text-[26px] font-montserrat tracking-[.20em]">MCX</p>
+                <p className="text-white text-[26px] font-montserrat font-black tracking-[.20em]">MCX</p>
             </div>
             <img src={logo} alt="Cart" className="max-h-14" />            
         </div>
