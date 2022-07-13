e5ad389e7d1d9ec7feb028abb66e6e5cc64a487b
Erick Marinho <erickgaves@gmail.com>
2022-07-13 12:48:36 -0300

    :sparkles: Header responsividade

--- mcx-icommgroup/src/Components/Header/Header.js
+++ mcx-icommgroup/src/Components/Header/Header.js
@@ -4,12 +4,12 @@ import mask from "../../assets/MaskGroup.png"
 
 function Header() {
     return (
-        <div className="flex justify-around bg-[#444444] h-[90px] items-center">
+        <div className="flex bg-[#444444] h-[90px] items-center lg:justify-around justify-between">
             <div className="flex flex-row items-center">
-                <img src={mask} alt="mask" className="max-h-7 flex items-center pr-2.5" />
+                <img src={mask} alt="mask" className="max-h-7 flex items-center pr-2.5 pl-2.5" />
                 <p className="text-white text-[26px] font-montserrat tracking-[.20em]">MCX</p>
             </div>
-            <img src={logo} alt="Cart" className="max-h-14" />
+            <img src={logo} alt="Cart" className="max-h-14" />            
         </div>
     )
 } 
