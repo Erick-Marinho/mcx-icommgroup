29d6f6d6e28a0c252072f76425332bd3b6030d4c
Erick Marinho <erickgaves@gmail.com>
2022-07-13 17:09:43 -0300

    :iphone: Responsividade componente

--- mcx-icommgroup/src/Components/GalleryImages/GalleryImages.js
+++ mcx-icommgroup/src/Components/GalleryImages/GalleryImages.js
@@ -3,15 +3,15 @@ import image6 from '../../assets/image6.png';
 
 function GalleryImages() {
     return (
-        <div className="max-full flex justify-center pt-[30px]">
-            <div className="flex flex-row">
+        <div className="max-full flex justify-center lg:pt-[30px] pt-[20px] mx-[20px]">
+            <div className="flex lg:flex-row flex-col">
                 <div className="relative">
-                    <img src={image5} alt="modelo" className="pr-[10px] " />
-                    <div className="absolute w-[20px] text-[26px] font-bold tracking-[.20em] text-white bottom-16 translate-x-10 font-montserrat">ÍRIS VERÃO 2022</div>
+                    <img src={image5} alt="modelo" className="lg:pr-[10px] " />
+                    <div className="absolute w-[20px] lg:text-[26px] text-[20px] font-bold tracking-[.20em] text-white bottom-16 translate-x-8 font-montserrat">ÍRIS VERÃO 2022</div>
                 </div>
-                <div className="relative">
-                    <img src={image6} alt="modelo" className="pl-[10px]" />
-                    <div className="absolute w-[120px] text-[26px] font-bold tracking-[.20em] text-white bottom-20 translate-x-8 font-montserrat">LE LIS BLANC</div>
+                <div className="relative lg:pt-[0px] pt-[20px]">
+                    <img src={image6} alt="modelo" className="lg:pl-[10px]" />
+                    <div className="absolute w-[120px] lg:text-[26px] text-[20px] font-bold tracking-[.20em] text-white bottom-20 translate-x-8 font-montserrat">LE LIS BLANC</div>
                 </div>
             </div>
         </div>
